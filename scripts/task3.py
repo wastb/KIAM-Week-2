1c7a02841b5e23a2179f8e2fbc8232263871803a
Wasihun Tesfaye <wasihunpersonal@gmail.com>
2024-12-21 18:37:32 +0300

    "task 3modified"

--- scripts/task3.py
+++ scripts/task3.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def replacce_with_medain(df):  
+def replace_with_medain(df):  
         #Replace missing values with mean
         numerical_cols = ['TCP UL Retrans. Vol (Bytes)','TCP DL Retrans. Vol (Bytes)', 'Avg RTT DL (ms)',
                           'Avg RTT UL (ms)','Avg Bearer TP DL (kbps)','Avg Bearer TP UL (kbps)']
